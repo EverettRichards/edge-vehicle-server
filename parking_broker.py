997d36fc0d13627de2b6bc306908dcd7210e42fd
EverettRichards <ehrichards9@gmail.com>
2024-07-25 17:57:34 -0400

    Test3

--- parking_broker.py
+++ parking_broker.py
@@ -206,7 +206,7 @@ def getVerdict():
     # Display separator for verdict presentation
     if settings["show_verbose_output"]:
         print("-"*40)
-        print(f"Getting verdict #{getPurple(verdict_id)} (t=...{getCyan(np.round(NOW%10000,3))}s)")
+        print(f"Getting verdict #{getYellow(verdict_id)} (t=...{getCyan(np.round(NOW%10000,3))}s)")
         print("-"*40)
 
     for client in activeClients:
