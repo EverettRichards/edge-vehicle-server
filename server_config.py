f83d9bea37d30dab6c495149213958b27d0b3693
EverettRichards <ehrichards9@gmail.com>
2024-07-31 13:17:26 -0400

    ObjectTestbedV1

--- server_config.py
+++ server_config.py
@@ -1,7 +1,7 @@
 config = {
     "broker_IP":"localhost",
     "port_Num":1883,
-    "verdict_min_refresh_time": 0.5, # Min number of seconds before a new verdict can be submitted
+    "verdict_min_refresh_time": 0.62, # Min number of seconds before a new verdict can be submitted
     "oldest_allowable_data": 10, # Max number of seconds before data is considered too old
     "show_verbose_output": True,
     "reputation_increment": 0.005, # Amount to increment or decrement client reputation by when they make a right decision
